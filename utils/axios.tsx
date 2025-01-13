2b401e145d2019fbf86cf120410213cd70cd9699
Dharmedra <dp903604@gmail.com>
2025-01-13 16:14:42 +0530

    change in axios

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -3,12 +3,9 @@
 import axios from "axios";
 
 const loginapp = axios.create({
-  // baseURL: "https://e-commerce-backend-g96o.onrender.com/",
-  baseURL: "http://localhost:4999/",
+  baseURL: "https://e-commerce-backend-g96o.onrender.com/",
+  // baseURL: "http://localhost:4999/",
   withCredentials: true,
-  // headers: {
-  //   Authorization: token ? token : "",
-  // },
 });
 
 export default loginapp;
